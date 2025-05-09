de9da0ee26c7418808a5ba4c3d387653007071f2
berica22 <ericabazoar@gmail.com>
2025-05-09 18:25:58 -0300

    subir tp 4

---
+++ 04 Estructuras Repetitivas/progrmacion-1-bustamante-tp4.py
@@ -0,0 +1,22 @@
+# Ejervivio 1
+# Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+#declaraos la variable de inicio del contador
+cont = 0
+# indicamos la cantidad de veces que se debe ejecutar el ciclo
+while cont <= 100: 
+    print(cont)
+# Declaramos el rango que ira aumentando la variable
+    cont += 1
+
+# Desarrolla un programa que solicite al usuario un número entero y 
+# determine la cantidad de dígitos que contiene.
+
+# Ejercicio 3 Escribe un programa que sume todos los números enteros 
+# comprendidos entre dos valores dados por el usuario, excluyendo esos dos valores.
+
+
+# Ejercicio 4 Elabora un programa que permita al usuario ingresar números enteros y
+# los sume en secuencia. El programa debe detenerse y mostrar el total acumulado cuando 
+# el usuario ingrese un 0.
